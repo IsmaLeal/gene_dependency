1205a86f35f67bb5e610f2931e03ec9bc3988646
Ismael Leal <70777838+IsmaLeal@users.noreply.github.com>
2025-03-06 13:48:38 +0000

    Update fraction_complexes.py

--- graph/fraction_complexes.py
+++ graph/fraction_complexes.py
@@ -66,20 +66,22 @@ def process_all_complexes(threshold=0.20, num_iterations=10000):
         - cell_line (str): The cell line associated with the complex.
 	- subunits(Gene name) (str): Names of genes in the complex.
  	- pval (float): Raw p-value.
-  	- corrected_pval (float): Adjusted p-value after multiple hypothesis correction.
-   	- significant (bool): True if the p-value is below the 0.05 significance threshold.
+  	- corrected_pval_by (float): Adjusted p-value after Benjamini-Yekutieli (BY) false discover rate.
+   	- significant_by (bool): True if the BY p-value is below the 0.05 significance threshold.
+  	- corrected_pval_bh (float): Adjusted p-value after Benjamini-Hochberg (BH) false discover rate.
+   	- significant_bh (bool): True if the BH p-value is below the 0.05 significance threshold.
     	- observed_edr (float): Computed EDR for the complex.
      	- num_genes (int): Number of genes present in the dataset for this complex.
       	- all_genes_present (bool): True if all genes in the CORUM complex are found in the CRISPR dataset.
        	- null_ratios (str): A string representation of a list with the null distribution of EDR values.
     """
     # Load dataset and preprocessed complexes
-    df = pd.read_csv('../datasets/filtered_complexes.csv')
+    df = pd.read_csv("../datasets/filtered_complexes.csv")
     complexes = load_CORUM()
     
     # Create graph from rank-normalised correlation matrix
     g = prep_graph(threshold, ranked=False)
-    gene_names = np.array(g.vertex_properties['names'])
+    gene_names = np.array(g.vertex_properties["names"])
     
     # Prepare lists to store results
     results = [process_complex(complex, gene_names, g, num_iterations) for complex in tqdm(complexes)]
@@ -88,26 +90,26 @@ def process_all_complexes(threshold=0.20, num_iterations=10000):
     observed_edrs, p_values, null_distributions, n_genes, all_genes = zip(*results)
     
     # Add results to dataframe
-    df['all_genes_present'] = all_genes
-    df['observed_edr'] = observed_edrs
-    df['pval'] = p_values
-    df['null_ratios'] = null_distributions
-    df['num_genes'] = n_genes
+    df["all_genes_present"] = all_genes
+    df["observed_edr"] = observed_edrs
+    df["pval"] = p_values
+    df["null_ratios"] = null_distributions
+    df["num_genes"] = n_genes
     
     # Remove complexes that couldn't be processed
     df = df.dropna()
 
-    # Apply multiple hypothesis correction
-    reject, p_corrected, _, _ = multipletests(df['pval'].values, alpha=0.05, method='fdr_by')
-    df['significant'] = reject
-    df['corrected_pval'] = p_corrected
-    
-    # Reorder columns and apply further adaptations
-    df = df[['complexID', 'cell_line', 'subunits(Gene name)', 'pval', 'corrected_pval',
-             'significant', 'observed_edr', 'num_genes', 'all_genes_present', 'null_ratios']]
-    
-    df = adapt_df(df)
-    
+    # Apply multiple hypothesis corrections
+    reject_by, p_corrected_by, _, _ = multipletests(df["pval"].values, alpha=0.05, method="fdr_by")
+    df["significant_by"] = reject_by
+    df["corrected_pval_by"] = p_corrected_by
+    reject_bh, p_corrected_bh, _, _ = multipletests(df["pval"].values, alpha=0.05, method="fdr_bh")
+    df["significant_bh"] = reject_bh
+    df["corrected_pval_bh"] = p_corrected_bh
+
+    # Reorder columns
+    df = df[["complexID", "cell_line", "subunits(Gene name)", "pval", "corrected_pval_by", "significant_by",
+             "corrected_pval_bh", "significant_bh", "observed_edr", "num_genes", "all_genes_present", "null_ratios"]]    
     return df
 
 
