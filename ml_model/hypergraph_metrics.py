6ab749a938df63a8fff6b7ebbc316d16da2aca4d
Ismael Leal <70777838+IsmaLeal@users.noreply.github.com>
2025-03-21 11:57:20 +0000

    Delete ml_model/hypergraph_metrics.py

--- ml_model/hypergraph_metrics.py
+++
@@ -1,9 +0,0 @@
-from functions import *
-
-mptn = map_pathway_to_nodes()
-
-hypergraphs = prep_hypergraphs(mptn)
-
-metrics = obtain_hypergraph_metrics(hypergraphs)
-
-metrics.to_csv('results/hg_metrics_final.csv', index=False)
